b484a6e3cd35cf84819096f03d574d7a2c300ee8
Peter Hahn <peterh@jauntvr.com>
2016-01-06 16:48:05 -0800

    cscope ctags gen files

--- bin/cpenv.sh
+++ bin/cpenv.sh
@@ -6,6 +6,11 @@ cp ~/.bash_profile $homedir/.bash_profile
 cp ~/.gvimrc $homedir/.gvimrc
 cp ~/.vimrc $homedir/.vimrc
 cp ~/.ctags $homedir/.ctags
+cp ~/ctags.cnf $homedir/ctags.cnf
+
+mkdir $homedir/dev
+cp ~/dev/cscope-gen $homedir/dev
+cp ~/dev/ctags-filegen $homedir/dev
 
 cp -r ~/.vim  $homedir/.vim
 cp -r ~/bin $homedir/bin
