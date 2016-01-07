e44d3ebdbdfec8ad14c2f317c531d467cdbe33f6
Peter Hahn <pethah@gmail.com>
2016-01-06 16:05:14 -0800

    update cpenv.sh to remove viminfo

--- bin/cpenv.sh
+++ bin/cpenv.sh
@@ -5,7 +5,6 @@ cp ~/.bashrc $homedir/.bashrc
 cp ~/.bash_profile $homedir/.bash_profile
 cp ~/.gvimrc $homedir/.gvimrc
 cp ~/.vimrc $homedir/.vimrc
-cp ~/.viminfo $homedir/.viminfo
 cp ~/.ctags $homedir/.ctags
 
 cp -r ~/.vim  $homedir/.vim
